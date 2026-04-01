346fd9cdee543bb3f574ffeb9132489c678474da
nicolecheban <cmg4nk@virginia.edu>
2026-04-01 15:49:15 -0400

    installed the Maps Compose library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.play.services.maps)
+    implementation("com.google.maps.android:maps-compose:6.12.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
